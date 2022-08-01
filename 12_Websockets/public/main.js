fa2e068eb14e7d139e1ab4c5e305ba6221e2479e
Esteban Villalba <e.f.villalba@gmail.com>
2022-08-01 20:59:29 -0300

    Entrega clase 12 - WebSockets

---
+++ 12_Websockets/public/main.js
@@ -0,0 +1,62 @@
+const socket = io.connect();
+
+
+//Función para mostrar los mensajes que escribió/envió el usuario
+function showMessages(data) {
+    const html = data.map((el, index) => {
+        return(`<div class="d-flex justify-content-center">
+            <p class="styleMail">${el.email}</p>
+            <p class="styleFecha">[${el.fecha}]:</p>
+            <p class="styleMsj">${el.mensaje}</p> </div>`)
+    }).join(" ");
+    document.getElementById('mensajes').innerHTML = html;
+  }
+
+//Función para tomar y "emitir" el mensaje enviado por el usuario
+function setMensaje(e) {
+    const mensaje = {
+        email: document.getElementById('email').value,
+        fecha: (new Date).toLocaleDateString(),
+        mensaje: document.getElementById('mensaje').value
+    };
+    socket.emit('new-message', mensaje);    
+    return false;//corta la ejecución de la función
+}
+
+
+socket.on('productos', function(data) { listarProductos(data); });
+socket.on('messages', function(data) { showMessages(data); });
+
+//funcion que se encarga de presentarel listado de productos
+function listarProductos(data) { 
+    let html=""
+    if(data.length>0)
+  
+    {
+      
+     html += data.map((el, index) => {   
+        return(
+          `<div class="row d-flex align-items-center">
+                <div class="col">${el.title}</div>
+                <div class="col">${el.price}</div>
+                <div class="col"><img class="img-fluid w-25"  src=${el.thumbnail}></div>
+        </div>`
+        )
+    }).join(" ");
+    }
+    else
+    {
+          html = `<div class="row d-flex align-items-center">
+           <div class="col"><h2>No Hay Productos Cargados</h2></div>
+        </div>`
+       
+    }
+    document.getElementById('productos').innerHTML = html;
+  }
+
+    
+
+
+    
+
+
---
+++ 12_Websockets/server.js
@@ -0,0 +1,61 @@
+const { Server } = require("engine.io")
+const express = require("express")
+const app=express()
+const fetch = require('node-fetch');
+const {Server:HttpServer}= require("http")
+const {Server:IOServer}= require("socket.io")
+
+
+
+const httpServer=new HttpServer(app)
+const io= new IOServer(httpServer)
+
+const Producto = require("./controllers/producto.controller");
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+//Implementación ejs
+app.set('views', './public');
+app.set('view engine', 'ejs');
+
+/////BEGIN CODIGO NUEVO POR TRABAJO 10
+app.get('/', (req, res) => {
+  const produc = Producto.getAll(); 
+  res.render('form', {produc});
+})
+
+app.get("/productos", (req, res) => {
+  const produc = Producto.getAll();  
+
+  res.status(200).json(produc); 
+});
+
+app.post('/productos',(req,res)=>{  
+  const { title, price,thumbnail } = req.body;
+  const produc = Producto.create(title, price,thumbnail);    
+  
+  res.redirect('/');  
+})
+
+httpServer.listen(8080,()=>{
+    console.log("Server on Port 8080")
+    
+})
+
+
+const messages = [ ];
+
+app.use(express.static("public"));
+
+io.on('connection',socket => {
+    console.log('Un cliente se ha conectado');
+    //Obtengo los productos con un Fetch y los devuelvo en un json a través de io.sockets
+    fetch("http://localhost:8080/productos")
+    .then(response => response.json())
+    .then(data => {io.sockets.emit('productos', data)}); 
+
+    socket.on('new-message',data => {
+      messages.push(data);
+      io.sockets.emit('messages', messages);
+    });
+
+ });
