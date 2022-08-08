0cb3be2a4441d5221ae7167062421f54e4081c09
Esteban Villalba <e.f.villalba@gmail.com>
2022-08-07 23:27:39 -0300

    Ajustes Entrega Final

--- 14_Primer_Entrega_Proyecto_Final/carritosBank.js
+++ 14_Primer_Entrega_Proyecto_Final/carritosBank.js
@@ -1,116 +1,110 @@
 /*const { getByID } = require("./controllers/producto.controller");
 const productosRouter = require("./routes/productosRouter");*/
 const prodBank = require("./productosBank");
-const data = [];
-const carritos=[]
+const fs = require("fs")
+
+const carritos = []
+
+const archivo = "./archivos/carritos.txt"
 let id = 0;
 
 const list = () => {
-   return carritos;
+  return carritos;
 }
 const findOne = (id) => {
-  const carrito =carritos.find((carrito) => parseInt(carrito.id) === parseInt(id));
-  if(carrito)
-  {return carrito.id} else {return { error : 'carrito no encontrado' }}
+  const carrito = carritos.find((carrito) => parseInt(carrito.id) === parseInt(id));
+  if (carrito) { return carrito.id } else { return { error: 'carrito no encontrado' } }
 };
 //Agrega un nuevo carrito
 const add = () => {
-  let timestamp= new Date().getTime();
-  /*let prodCarr=[]
-  for ( const prod of productos)
-  {
-    console.log("For Of add Prods",prod)
-    prodCarr.push(prodBank.findOne(prod))
-  }*/
-  const carrito = { id: ++id,timestamp,productos : []};
+  let timestamp = new Date().getTime();
+  const carrito = { id: ++id, timestamp, productos: [] };
   carritos.push(carrito);
+  try {
+    fs.writeFileSync(archivo, JSON.stringify(carritos))
+  }
+  catch (err) {
+    console.log(err)
+  }
   return carrito.id;
 };
 //Agrega productos a un carrito
-const addProductos = (idCarrito,productos) => {  
-  const carIndex =carritos.findIndex((carrito) => parseInt(carrito.id) === parseInt(idCarrito));
-  if(carIndex>=0)
-  {
-        for ( const prod of productos)
-        {
-          console.log("For Of add Prods",prod)
-          console.log("Carr.id",carIndex)
-          carritos[carIndex].productos.push(prodBank.findOne(prod))
-        }
-        return 'Productos Agregados';      
+const addProductos = (idCarrito, productos) => {
+  const carIndex = carritos.findIndex((carrito) => parseInt(carrito.id) === parseInt(idCarrito));
+  if (carIndex >= 0) {
+    for (const prod of productos) {
+      console.log("For Of add Prods", prod)
+      console.log("Carr.id", carIndex)
+      carritos[carIndex].productos.push(prodBank.findOne(prod))
+    }
+    try {
+      fs.writeFileSync(archivo, JSON.stringify(carritos))
+    }
+    catch (err) {
+      console.log(err)
+    }
+    return 'Productos Agregados';
   }
-  else{
+  else {
     return 'No Existe el Carrito';
   }
 };
 //Elimina un carrito y sus productos
-const remove = (idCarrito) => {    
-  const carIndex =carritos.findIndex((carrito) => parseInt(carrito.id) === parseInt(idCarrito));  
-  if(carIndex>=0)
-  {    
-    carritos.splice(carIndex,1)   
-    return "Carrito " + idCarrito .toString() + " Eliminado"  
-  }  
-  else {return { error : 'carrito no encontrado para eliminar' }}  
+const remove = (idCarrito) => {
+  const carIndex = carritos.findIndex((carrito) => parseInt(carrito.id) === parseInt(idCarrito));
+  if (carIndex >= 0) {
+    carritos.splice(carIndex, 1)
+    try {
+      fs.writeFileSync(archivo, JSON.stringify(carritos))
+    }
+    catch (err) {
+      console.log(err)
+    }
+    return "Carrito " + idCarrito.toString() + " Eliminado"
+  }
+  else { return { error: 'carrito no encontrado para eliminar' } }
 };
 //Devuelve los productos de un carrito
-const findProductosCarrito = (idCarrito) => {    
-  const carIndex =carritos.findIndex((carrito) => parseInt(carrito.id) === parseInt(idCarrito));  
-  if(carIndex>=0)
-  {    
+const findProductosCarrito = (idCarrito) => {
+  const carIndex = carritos.findIndex((carrito) => parseInt(carrito.id) === parseInt(idCarrito));
+  if (carIndex >= 0) {
     return carritos[carIndex].productos
-  }  
-  else {return { error : 'carrito no encontrado' }}  
+  }
+  else { return { error: 'carrito no encontrado' } }
 };
 
 //Elimina un Producto de un carrito
-const removeProductoCarrito = (idCarrito,idProducto) => {  
-  const carIndex =carritos.findIndex((carrito) => parseInt(carrito.id) === parseInt(idCarrito));
-  if(carIndex>=0)
-  { 
-    const prodIndex =carritos[carIndex].productos.findIndex((producto) => parseInt(producto.id) === parseInt(idProducto));
-    
-    if(prodIndex>=0)
-    { 
-      carritos[carIndex].productos.splice(prodIndex,1)
-      return " Se eliminó Producto "+ idProducto.toString() +" del carrito "+ idCarrito.toString() 
-    }  
-    else
-    {
-      return { error : 'producto no encontrado en el carrito' }
-    }  
+const removeProductoCarrito = (idCarrito, idProducto) => {
+  const carIndex = carritos.findIndex((carrito) => parseInt(carrito.id) === parseInt(idCarrito));
+  if (carIndex >= 0) {
+    const prodIndex = carritos[carIndex].productos.findIndex((producto) => parseInt(producto.id) === parseInt(idProducto));
+
+    if (prodIndex >= 0) {
+      carritos[carIndex].productos.splice(prodIndex, 1)
+      try {
+        fs.writeFileSync(archivo, JSON.stringify(carritos))
+      }
+      catch (err) {
+        console.log(err)
+      }
+      return " Se eliminó Producto " + idProducto.toString() + " del carrito " + idCarrito.toString()
+    }
+    else {
+      return { error: 'producto no encontrado en el carrito' }
+    }
   }
-  else {return { error : 'carrito no encontrado' }}  
-};
-
-        
-
-
-const update = (id, newTitle,newPrice,newThumbnail) => {  
-  const actualizar = carritos.findIndex(carr=>parseInt(carr.id) === parseInt(id))      
-  if (actualizar>-1)
-  { 
-    const carrito=findOne(id)
-    if (newTitle) {carrito.title=newTitle;}
-    if (newPrice) { carrito.price = newPrice;}
-    if (newThumbnail) { carrito.thumbnail = newThumbnail;}    
-    return carrito
-  }
-  else {return { error : 'producto no encontrado para actualizar' }}  
+  else { return { error: 'carrito no encontrado' } }
 };
 
 
-module.exports = { list,add,addProductos,findOne,remove,update,findProductosCarrito,removeProductoCarrito};
-/*Ejemplo objetos de array produc
-{
-  "title": "PEPE3",
-  "price": "RAZA3",
-  "thumbnail": "IMAGEN3"
- }*/
+
+
+module.exports = { list, add, addProductos, findOne, remove, findProductosCarrito, removeProductoCarrito };
+
 const carrito = [
 
 ];
 
 for (let i = 0; i < carrito.length; i++) {
-  module.exports.add(carrito[i].title, carrito[i].price,carrito[i].thumbnail);
+  module.exports.add(carrito[i].id, carrito[i].timestamp, carrito[i].productos);
 }
--- 14_Primer_Entrega_Proyecto_Final/productosBank.js
+++ 14_Primer_Entrega_Proyecto_Final/productosBank.js
@@ -1,53 +1,69 @@
 //const { getByID } = require("./controllers/producto.controller");
 //const productosRouter = require("./routes/productosRouter");
+const fs = require("fs")
 
 const data = [];
 let id = 0;
+const archivo = "./archivos/productos.txt"
 
 const list = () => {
-   return data;
+  return data;
 }
 const findOne = (id) => {
-  console.log("Id findOne",id)
-  console.log("Data",data)
-  const produc =data.find((produc) => parseInt(produc.id) === parseInt(id));
-  if(produc)
-  {return produc} else {return { error : 'producto no encontrado' }}
+  const produc = data.find((produc) => parseInt(produc.id) === parseInt(id));
+  if (produc) { return produc } else { return { error: 'producto no encontrado' } }
 };
 
 const add = (nombre, descripcion, codigo, foto, precio, stock) => {
-  let timestamp= new Date().getTime();
-  const produc = { id: ++id, timestamp,nombre, descripcion, codigo, foto, precio, stock };
+  let timestamp = new Date().getTime();
+  const produc = { id: ++id, timestamp, nombre, descripcion, codigo, foto, precio, stock };
   data.push(produc);
+  try {
+    fs.writeFileSync(archivo, JSON.stringify(data))
+  }
+  catch (err) {
+    console.log(err)
+  }
   return produc;
 };
 const remove = (id) => {  
-  const eliminar = data.findIndex(prod=>parseInt(prod.id) === parseInt(id))  
-  if (eliminar>-1){
-    const produc= data.splice(data.findIndex(prod=>parseInt(prod.id) === parseInt(id)),1)
-    return produc
-  }  
-  else {return { error : 'producto no encontrado para eliminar' }}  
-};
-
-const update = (id, newNombre, newDescripcion, newCodigo, newFoto, newPrecio, newStock) => {  
-  const actualizar = data.findIndex(prod=>parseInt(prod.id) === parseInt(id))      
-  if (actualizar>-1)
-  { 
-    const produc=findOne(id)
-    if (newNombre) {produc.nombre=newNombre;}
-    if (newDescripcion) { produc.descripcion = newDescripcion;}
-    if (newCodigo) { produc.codigo = newCodigo;}    
-    if (newFoto) { produc.codigo = newFoto;}    
-    if (newPrecio) { produc.precio = newPrecio;}    
-    if (newStock) { produc.stock = newStock;}    
+  const eliminar = data.findIndex(prod => parseInt(prod.id) === parseInt(id))
+  if (eliminar > -1) {
+    const produc = data.splice(data.findIndex(prod => parseInt(prod.id) === parseInt(id)), 1)
+    try {
+      fs.writeFileSync(archivo, JSON.stringify(data))
+    }
+    catch (err) {
+      console.log(err)
+    }
     return produc
   }
-  else {return { error : 'producto no encontrado para actualizar' }}  
+  else { return { error: 'producto no encontrado para eliminar' } }
+};
+
+const update = (id, newNombre, newDescripcion, newCodigo, newFoto, newPrecio, newStock) => {
+  const actualizar = data.findIndex(prod => parseInt(prod.id) === parseInt(id))
+  if (actualizar > -1) {
+    const produc = findOne(id)
+    if (newNombre) { produc.nombre = newNombre; }
+    if (newDescripcion) { produc.descripcion = newDescripcion; }
+    if (newCodigo) { produc.codigo = newCodigo; }
+    if (newFoto) { produc.codigo = newFoto; }
+    if (newPrecio) { produc.precio = newPrecio; }
+    if (newStock) { produc.stock = newStock; }
+    try {
+      fs.writeFileSync(archivo, JSON.stringify(data))
+    }
+    catch (err) {
+      console.log(err)
+    }
+    return produc
+  }
+  else { return { error: 'producto no encontrado para actualizar' } }
 };
 
 
-module.exports = { list,add,findOne,remove,update};
+module.exports = { list, add, findOne, remove, update };
 /*Ejemplo objetos de array produc
 {
         "nombre": "UNO",
@@ -62,5 +78,5 @@ const produc = [
 ];
 
 for (let i = 0; i < produc.length; i++) {
-  module.exports.add(produc[i].nombre, produc[i].descripcion,produc[i].codigo,produc[i].foto, produc[i].precio,produc[i].stock);
+  module.exports.add(produc[i].nombre, produc[i].descripcion, produc[i].codigo, produc[i].foto, produc[i].precio, produc[i].stock);
 }
